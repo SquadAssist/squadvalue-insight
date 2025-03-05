8ab701c7b730a88d66995b7999f40926e94b9bcc
WoutPauwels <47983129+WoutPauwels@users.noreply.github.com>
2025-03-05 20:50:49 +0100

    Update Contact.tsx with new trial for fixing things

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -35,16 +35,28 @@ const Contact = () => {
     };
 
     try {
-
+        emailjs.init({
+          publicKey: EMAILJS_PUBLIC_KEY,
+          // Do not allow headless browsers
+          blockHeadless: true,
+          blockList: {
+            // Block the suspended emails
+            list: ['foo@emailjs.com', 'bar@emailjs.com'],
+            // The variable contains the email address
+            watchVariable: 'userEmail',
+          },
+          limitRate: {
+            // Set the limit rate for the application
+            id: 'app',
+            // Allow 1 request per 10s
+            throttle: 10000,
+          },
+      });
       // Send email using EmailJS
       await emailjs.send(
         EMAILJS_SERVICE_ID,
         EMAILJS_TEMPLATE_ID,
-        templateParams,
-        {
-        publicKey: EMAILJS_PUBLIC_KEY,
-        privateKey: EMAILJS_PRIVATE_KEY
-        }
+        templateParams
         //EMAILJS_USER_ID
       );
       
